1cac7d9cc2b6f05de355b61d29dabb231bb5e36a
Bruce Parrello <bparrello@figresearch.com>
2019-07-07 21:47:57 -0500

    Initial configuration

---
+++ src/main/java/org/theseed/proteins/cluster/RoleClusterCounter.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package org.theseed.proteins.cluster;
+
+import org.theseed.counters.PairCounter;
+import org.theseed.proteins.Role;
+
+/**
+ * This class counts function
+ * @author Bruce Parrello
+ *
+ */
+public class RoleClusterCounter extends PairCounter<Role> {
+
+}
